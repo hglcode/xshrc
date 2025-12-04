e95c2cf360135751816813715e9722c1295efddb
sam <hgl9@foxmail.com>
2025-12-04 21:47:33 +0800

    feat: add zephyr sdk tool env path.

--- env.sh
+++ env.sh
@@ -11,12 +11,13 @@ export PATH="$PNPM_HOME:$PATH"
 # go
 export GO111MODULE=on
 export GOPROXY=https://goproxy.cn
-export GOPATH=~/.local/app/go
-
-# ruby
-export PATH=~/bin:"$PATH"
+export GOPATH=/media/work/.app/go
 
 # fcitx
 export GTK_IM_MODULE=fcitx
 export QT_IM_MODULE=fcitx
 export XMODIFIERS=@im=fcitx
+
+# ZEPHYR
+export ZEPHYR_GCC_VARIANT=zephyr
+export ZEPHYR_SDK_INSTALL_DIR=/media/work/dev/sdk/zephyr
